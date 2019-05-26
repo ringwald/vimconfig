c8b6ca24b383fc63b194aadd2239085190e5c76b
Steven Ringwald <steven@ringwald.com>
2019-05-25 20:54:17 -0700

    Initial checking of scripts.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+mkdir -p pack/asric/{opt,start}
+
+touch pack/.gitkeep pack/asric/opt/.gitkeep pack/asric/start/.gitkeep pack/asric/.gitkeep
+
+git init
+git submodule init
+
+git submodule add https://github.com/tpope/vim-fugitive.git pack/asric/start/vim-fugitive
+
